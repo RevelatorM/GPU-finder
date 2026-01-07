38c7d7261308b5528f785a282f295e2b12c74016
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-07 19:03:31 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "modernc.org/sqlite"
 )
@@ -24,10 +25,29 @@ func main() {
 		return
 	}
 	//===================
-	stmt, _ := database.Prepare("CREATE IF NOT EXISTS gpus (id INTEGER PRIMARY KEY, gpuname TEXT, vram INTEGER, MHz TEXT, vmhz TEXT)")
+	stmt, _ := database.Prepare("CREATE TABLE IF NOT EXISTS gpus (id INTEGER PRIMARY KEY, gpuname TEXT, vram TEXT, MHz TEXT, vmhz TEXT)")
+	stmt.Exec()
+
+	stmt, _ = database.Prepare("INSERT INTO gpus(gpuname,vram,MHz,vmhz) VALUES (?,?)")
 	//===================
 	for {
 		fmt.Print("Welcome to GPU finder,type in GPU`s name to find: \n")
+		fmt.Print("or type 0 to cose: \n")
+		//===================
+		var choice int
+		fmt.Scan(&choice)
 
+		switch choice {
+		case 1:
+			addTask(database)
+		case 2:
+			showTask(database)
+		case 3:
+			deleteTask(database)
+		case 0:
+			os.Exit(0)
+		default:
+			fmt.Println("error")
+		}
 	}
 }
