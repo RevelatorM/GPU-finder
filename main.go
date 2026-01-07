8d060e554e9f4506ac16a740e324b24eb237889d
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-07 18:27:39 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//===================
+type GPU struct {
+	Name string
+	VRAM int
+	MHz  float32
+	vmhz float32
+}
+
+//===================
+
+func main() {
+	for {
+		fmt.Print("Welcome to GPU finder,type in GPU`s name to find: \n")
+
+	}
+}
