ddd01f258697b25969ef0bbbcd9756eb4418af73
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-07 18:50:30 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
 
-//===================
+	_ "modernc.org/sqlite"
+)
+
+// ===================
 type GPU struct {
 	Name string
 	VRAM int
@@ -13,6 +18,14 @@ type GPU struct {
 //===================
 
 func main() {
+	database, err := sql.Open("sqlite", "./database.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//===================
+	stmt, _ := database.Prepare("CREATE IF NOT EXISTS gpus (id INTEGER PRIMARY KEY, gpuname TEXT, vram INTEGER, MHz TEXT, vmhz TEXT)")
+	//===================
 	for {
 		fmt.Print("Welcome to GPU finder,type in GPU`s name to find: \n")
 
